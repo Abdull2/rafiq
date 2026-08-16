0a1465b6fb2d1932f0f8a37904397672870e9b41
Abdullah Mahmoud <30419658+Abdull2@users.noreply.github.com>
2026-08-17 00:39:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 const CACHE = 'rafiq-live';
 const ASSETS = [
   './', './index.html', './tasbih.html', './qalb.html', './qalb.json',
-  './azkar.json', './quran.json', './hafs.woff2',
+  './azkar.json', './adiya.json', './quran.json', './hafs.woff2',
   './manifest.webmanifest', './icon-192.png', './icon-512.png',
   './icon-maskable-512.png', './apple-touch-icon.png'
 ];
